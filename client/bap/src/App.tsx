4874fd95ea6759c5df5eed5b50ce02fd17a8f1c0
Ben Baldwin <benbaldwin000@gmail.com>
2023-05-21 13:38:45 -0700

    stuff

---
+++ client/bap/src/App.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+});
+
+export const App = () => (
+	<View style={styles.container}>
+		<Text>Hello world</Text>
+	</View>
+);
